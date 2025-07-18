f3f8eb46f3c4f45fb2d6a782358371e784a8733f
Darya <nastenko_1999@mail.ru>
2025-07-19 00:01:09 +0700

    2.3 Финальный макет

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,34 @@
-import {defineConfig} from "vite"
+import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+
 export default defineConfig({
-    base: "/Web-Core",
-} )
+    base: '/Web-Core/',
+    build: {
+        outDir: 'docs',
+        emptyOutDir: true,
+        assetsInlineLimit: 4096,
+        rollupOptions: {
+            output: {
+                assetFileNames: 'assets/[name]-[hash][extname]',
+                chunkFileNames: 'assets/[name]-[hash].js',
+                entryFileNames: 'assets/[name]-[hash].js'
+            }
+        }
+    },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `
+          @use "@/scss/global/variables" as *;
+          @use "@/scss/global/mixins" as *;
+        `,
+                charset: false
+            }
+        }
+    }
+})
